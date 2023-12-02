95190dbb86cad4e492500516320e1fd8af895f55
salehalmansour <144630823+salehalmansour@users.noreply.github.com>
2023-12-01 22:04:09 -0800

    Update LED.py
    code to control 2 LED's independent of each other

--- LED.py
+++ LED.py
@@ -4,18 +4,20 @@ import time                            #calling time to provide delays in progra
 
 #Led
 GPIO.setwarnings(False)
-GPIO.setmode (GPIO.BCM)         #we are programming the GPIO by BCM pin numbers. (PIN35 as ‘GPIO19’)
-GPIO.setup(12,GPIO.OUT)           # initialize GPIO19 as an output.
-p = GPIO.PWM(12,100)          #GPIO19 as PWM output, with 100Hz frequency
-p.start(0)                              #generate PWM signal with 0% duty cycle
-
+GPIO.setmode (GPIO.BCM)                          #we are programming the GPIO by BCM pin numbers. (PIN35 as ‘GPIO19’)
+GPIO.setup(12,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)                          # initialize GPIO19 as an output.
+LED_1 = GPIO.PWM(12,100)
+LED_2 = GPIO.PWM(13,100)                        #GPIO19 as PWM output, with 100Hz frequency
+LED_1.start(0)                                  #generate PWM signal with 0% duty cycle
+LED_2.start(0)
 
 def LED():
     i=0
     duty_cycle = 0
-    while 1:
+    while 1:					                #controll LED1
         
-        t=input ("press'e' to increase birghtness\npress 'q' to edecrease brightness\npress 's' to set\n")
+        t=input ("led_1\n	press'e' to increase birghtness\n	press 'q' to decrease brightness\n	press 's' to set\n")
         
         if t == 'e':        
             duty_cycle = duty_cycle + 10
@@ -23,11 +25,11 @@ def LED():
         if t == 'e' and i<100:
             i=i+10
             print("Duty Cycle Set to :%", i)
-            p.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+            LED_1.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
         
         elif duty_cycle > 100:
             print ("'Error Duty Cycle cant be more than %100'")
-            uty_cycle = duty_cycle - 10
+            duty_cycle = duty_cycle - 10
             print("Duty Cycle Set to:%", i)
             
         elif t == 'q':
@@ -36,13 +38,44 @@ def LED():
         if t == 'q' and i>0:
             i = i-10
             print("Duty Cycle Set to :%", i)
-            p.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+            LED_1.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
         
         elif duty_cycle < 0:
             print ("'Error Duty Cycle cant be less than %0'")
             print("Duty Cycle Set to :%", i)
             duty_cycle = duty_cycle + 10
         elif t == 's':
-            #main()                         # used in other file to display image
+            
             break;
-
+    while 1:			                    #CONTROLL Led_2
+        t=input ("LED_2\n	press'e' to increase birghtness\n	press 'q' to edecrease brightness\n	press 's' to set\n")
+        
+        if t == 'e':        
+            duty_cycle = duty_cycle + 10
+        
+        if t == 'e' and i<100:
+            i=i+10
+            print("Duty Cycle Set to :%", i)
+            LED_2.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+        
+        elif duty_cycle > 100:
+            print ("'Error Duty Cycle cant be more than %100'")
+            duty_cycle = duty_cycle - 10
+            print("Duty Cycle Set to:%", i)
+            
+        elif t == 'q':
+            duty_cycle = duty_cycle -10
+            print("Duty Cycle Set to:%", i)
+        if t == 'q' and i>0:
+            i = i-10
+            print("Duty Cycle Set to :%", i)
+            LED_2.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+        
+        elif duty_cycle < 0:
+            print ("'Error Duty Cycle cant be less than %0'")
+            print("Duty Cycle Set to :%", i)
+            duty_cycle = duty_cycle + 10
+        elif t == 's':
+            #main()                             # used in other file to display image
+            break;
+LED()                                            #this lin is for testing only. not included in the main code
