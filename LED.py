61b5669659489ec46e209caf8194c55c145b8334
salehalmansour <144630823+salehalmansour@users.noreply.github.com>
2023-12-04 01:28:38 -0800

    Update LED.py

--- LED.py
+++ LED.py
@@ -13,71 +13,53 @@ LED_1.start(0)                              #generate PWM signal with 0% duty cy
 LED_2.start(0)
 
 def LED():
-    i=0
-    duty_cycle = 0
+    i = 0
     while 1:					#controll LED1
-        
+        print("Duty Cycle Set to :%", i)
         t=input ("led_1\n	press'e' to increase birghtness\n	press 'q' to decrease brightness\n	press 's' to set\n")
         
-        if t == 'e':        
-            duty_cycle = duty_cycle + 10
-        
-        if t == 'e' and i<100:
+        if t == 'e': 
             i=i+10
-            print("Duty Cycle Set to :%", i)
-            LED_1.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+            if i < 100:
+                LED_1.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+            else:
+                i = 100
+                LED_1.ChangeDutyCycle(i)
+                print('ERROR Duty Cycle Cant be more than 100')
         
-        elif duty_cycle > 100:
-            print ("'Error Duty Cycle cant be more than %100'")
-            duty_cycle = duty_cycle - 10
-            print("Duty Cycle Set to:%", i)
-            
         elif t == 'q':
-            duty_cycle = duty_cycle -10
-            print("Duty Cycle Set to:%", i)
-        if t == 'q' and i>0:
             i = i-10
-            print("Duty Cycle Set to :%", i)
-            LED_1.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
-        
-        elif duty_cycle < 0:
-            print ("'Error Duty Cycle cant be less than %0'")
-            print("Duty Cycle Set to :%", i)
-            duty_cycle = duty_cycle + 10
+            if i > 0:
+                LED_1.ChangeDutyCycle(i)			#change duty cycle for varying the brightness of LED."""
+            else:
+                i = 0
+                LED_1.ChangeDutyCycle(i)
+                print("ERROR Duty Cycle Cant be less than 0")
         elif t == 's':
             break;
     
     i2=0
-    duty_cycle2 = 0
     while 1:			#CONTROLL Led_2
+        print("Duty Cycle Set to :%", i2)
         t=input ("LED_2\n	press'e' to increase birghtness\n	press 'q' to edecrease brightness\n	press 's' to set\n")
         
-        if t == 'e':        
-            duty_cycle2 = duty_cycle2 + 10
-        
-        if t == 'e' and i2<100:
+        if t == 'e': 
             i2=i2+10
-            print("Duty Cycle Set to :%", i2)
-            LED_2.ChangeDutyCycle(i2)			#change duty cycle for varying the brightness of LED."""
+            if i2 < 100:
+                LED_2.ChangeDutyCycle(i2)			#change duty cycle for varying the brightness of LED."""
+            else:
+                i2 = 100
+                LED_2.ChangeDutyCycle(i2)
+                print('ERROR Duty Cycle Cant be more than 100')
         
-        elif duty_cycle2 > 100:
-            print ("'Error Duty Cycle cant be more than %100'")
-            duty_cycle2 = duty_cycle2 - 10
-            print("Duty Cycle Set to:%", i2)
-            
         elif t == 'q':
-            duty_cycle2 = duty_cycle2 -10
-            print("Duty Cycle Set to:%", i2)
-        if t == 'q' and i2>0:
             i2 = i2-10
-            print("Duty Cycle Set to :%", i2)
-            LED_2.ChangeDutyCycle(i2)			#change duty cycle for varying the brightness of LED."""
-        
-        elif duty_cycle2 < 0:
-            print ("'Error Duty Cycle cant be less than %0'")
-            print("Duty Cycle Set to :%", i2)
-            duty_cycle2 = duty_cycle2 + 10
+            if i2 > 0:
+                LED_2.ChangeDutyCycle(i2)			#change duty cycle for varying the brightness of LED."""
+            else:
+                i2 = 0
+                LED_2.ChangeDutyCycle(i2)
+                print("ERROR Duty Cycle Cant be less than 0")
         elif t == 's':
-            #main()
-            break;
+            main()
 LED()
