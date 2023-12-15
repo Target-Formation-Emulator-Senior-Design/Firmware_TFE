3feb16aca0146a8e465552fa616e38f12891c093
chori0 <129703624+chori0@users.noreply.github.com>
2023-12-15 12:17:10 -0800

    Update update_start.py
    Added init at start
    Added comments

--- update_start.py
+++ update_start.py
@@ -1,3 +1,38 @@
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.lcd.device import st7567
+import RPi.GPIO as GPIO
+from pathlib import Path
+#import urllib3
+from PIL import Image
+import os
+import time
+#import matplotlib.pyplot as plt
+import PIL.ImageOps
+from gpiozero import CPUTemperature
+import datetime
+import Adafruit_DHT
+import sys
+#Led
+GPIO.setwarnings(False)
+GPIO.setmode (GPIO.BCM)         	#we are programming the GPIO by BCM pin numbers. (PIN35 as ‘GPIO19’)
+GPIO.setup(12,GPIO.OUT)           	# initialize GPIO19 as an output.
+GPIO.setup(13,GPIO.OUT)				# initialize GPIO19 as an output.
+LED_1 = GPIO.PWM(12,10000)          		#GPIO19 as PWM output, with 10000Hz frequency
+LED_2 = GPIO.PWM(13,10000)			#GPIO19 as PWM output, with 10000Hz frequency
+
+
+i =50
+i2=50
+#         
+LED_1.start(i)                          #generate PWM signal with 0% duty cycle                             
+LED_2.start(i2)                      
+       
+#lcd
+serial = spi(port=0, device=0, gpio_DC=23, gpio_RST=17)
+device = st7567(serial, rotate=0)
+lcdFreq = 5
+
 def main():
    
    #instead of processing all files with each loop, we're going pre-process the images and save it in a new folder
@@ -30,6 +65,7 @@ def main():
         
         cpu = CPUTemperature()
         date = datetime.datetime.now()
+       #Opening and saving to this file might slow down the loop. The file size grows linearly and probably adds a delay
         with open ('/var/www/html/CPUTemp.csv','a') as datafile:
             datafile.write(str(cpu.temperature)+"\n")
         with open ('/var/www/html/Data2.csv','a') as datafile:
@@ -47,10 +83,9 @@ def main():
 
 
             for image_file in image_files:
-                
+                #this catch will reset the time each loop so the time.sleep function can properly subtrack
                 if start_time == 0:
                     start_time = time.time()
-
                 image_path = os.path.join(image_directory, image_file)
                 img = Image.open(image_path)
                 device.display(img)
